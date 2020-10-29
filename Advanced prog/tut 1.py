75784728d7959ea8200a9c96c8f529cb9dd39fc4
FAdy200 <abuyehia200@gmail.com>
2020-10-29 17:33:08 +0200

    new year yay

---
+++ Advanced prog/tut 1.py
@@ -0,0 +1,8 @@
+print(([0]+list(map(len, input().split())))[-1])
+
+# class Solution:
+#     def lengthOfLastWord(self, s: str) -> int:
+#         return (([0]+list(map(len, s.split())))[-1])
+
+# Solution.lengthOfLastWord(input())
+
