d02874fa4abce25e24c15e27c7c35f83262d1295
FAdy200 <abuyehia200@gmail.com>
2020-12-01 10:30:58 +0200

    new year yay

---
+++ Advanced_prog/Exercises_for_lab_6/FadiAlahmadAlomar_Lab5.py
@@ -0,0 +1,87 @@
+# CSE314- Lab 5
+#
+# Topic: Design patterns II
+# Author : Fadi Alahmad
+# Date : 01/ 12/ 2020
+# Problem 1:
+class Duck:
+    def quack(self):
+        print("Quack")
+
+    def fly(self):
+        print("I'm flying")
+
+
+class Turkey:
+    def gobble(self):
+        print("Gobble Gobble")
+
+    def fly(self):
+        print("I'm flying a short distance")
+
+
+class TurkeyAdapter:
+    """
+    this class takes the turkey class and adapts it to the duck calss
+    """
+    def __init__(self, adaptee):
+        """
+
+        :param adaptee: Turkey instance
+        """
+        self.adaptee = adaptee
+
+    def quack(self):
+        """
+        :calls: gobble function of the adaptee which is a Turkey instance
+        """
+        self.adaptee.gobble()
+
+    def fly(self):
+        """
+        :calls: 5 times the fly function of the adaptee which is a Turkey instance
+        """
+        self.adaptee.fly()
+        self.adaptee.fly()
+        self.adaptee.fly()
+        self.adaptee.fly()
+        self.adaptee.fly()
+
+
+def duck_interaction(duck):
+    duck.quack()
+    duck.fly()
+
+
+duck = Duck()
+turkey = Turkey()
+turkey_adapter = TurkeyAdapter(turkey)
+
+print("The Turkey says")
+turkey.gobble()
+turkey.fly()
+
+print("\nThe Duck says")
+duck.quack()
+duck.fly()
+
+print("\nthe Turkey_Adapter says")
+duck_interaction(turkey_adapter)
+"""
+Expected output:
+The Turkey says
+Gobble Gobble
+I'm flying a short distance
+
+The Duck says
+Quack
+I'm flying
+
+the Turkey_Adapter says
+Gobble Gobble
+I'm flying a short distance
+I'm flying a short distance
+I'm flying a short distance
+I'm flying a short distance
+I'm flying a short distance
+"""
---
+++ Advanced_prog/Exercises_for_lab_6/intro_to_GUI.py
@@ -0,0 +1,14 @@
+import tkinter as tk
+from tkinter import ttk as tkk
+parent = tk.Tk()
+# parent.title("welcome my dear friend")
+# my_label = tk.Label(parent,text="label")
+# botton = tk.Button(parent,text = "yaayy")
+# botton.grid(column = 2,row = 2)
+# my_label.grid(column=10, row=10)
+# parent.mainloop()
+
+s = tk.StringVar()
+c = tkk.Combobox(parent,textvariable  = s)
+
+
