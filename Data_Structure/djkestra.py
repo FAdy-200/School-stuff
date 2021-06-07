5a6cf7b95508449ac57f233fcca39c024586e2d5
FAdy200 <abuyehia200@gmail.com>
2021-06-07 14:38:04 +0200

    djkestra updated to find shortest from all nodes and print them

--- Data_Structure/djkestra.py
+++ Data_Structure/djkestra.py
@@ -1,39 +1,39 @@
 import networkx
 import math
+from prettytable import PrettyTable as table
 
-g = networkx.DiGraph()
-d = dict()
-for i in "ABCDEFGH":
+g = networkx.Graph()
+for i in "xzyvtuw":
     g.add_node(i)
-    d[i] = [False, math.inf, None]
-g.add_weighted_edges_from([("A", "B", 2), ("A", "D", 4), ("A", "C", 1), ("B", "F", 2), ("B", "C", 5), ("B", "E", 10),
-                           ("C", "A", 9), ("C", "E", 11), ("D", "C", 2), ("E", "D", 7), ("E", "G", 1), ("F", "H", 3),
-                           ("H", "G", 1), ("G", "F", 2), ("G", "E", 3)])
-
-print(g["B"])
+g.add_weighted_edges_from([("z", "y", 12), ("z", "x", 8), ("x", "y", 6), ("x", "v", 3), ("x", "w", 6), ("y", "v", 8),
+                           ("y", "t", 7), ("v", "t", 4), ("v", "w", 4), ("v", "u", 3), ("w", "u", 3), ("u", "t", 2)])
 
 
-def djkesrtra(gr, n, dist, d):
+
+def djkesrtra(gr, n, t):
+    d = dict()
+    for i in g.nodes:
+        d[i] = [False, math.inf, None]
     d[n] = [True, 0, None]
     s = 1
     while s < len(d):
         smallest = [None, math.inf]
         for i in gr[n].keys():
-            if not d[i][0]:
+            if not d[i][0] and d[i][1]>gr[n][i]["weight"] + d[n][1]:
                 d[i] = [False, gr[n][i]["weight"] + d[n][1], n]
         for j, i in d.items():
             if i[1] < smallest[1] and not i[0]:
                 smallest = [j, i[1]]
-
+        k = [s]
+        for i in d.keys():
+            k.append(d[i][2:0:-1])
+        t.add_row(k)
         d[smallest[0]][0] = True
         n = smallest[0]
         s += 1
 
-
-x = "E"
-djkesrtra(g, "A", x, d)
-print(d[x][1])
-while x is not None:
-    print(x, end="<-")
-    x = d[x][2]
-print(d)
+for i in g.nodes:
+    t = table(list("#"+"xzyvtuw"))
+    t.title = i+"'s table"
+    djkesrtra(g, i, t)
+    print(t)
